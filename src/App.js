04582686ccca952f52e83649c997d8b56a07c0dd
Nam Nguyen <namnm1991@gmail.com>
2020-10-01 13:51:35 +0700

    Add Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+
 import './App.css';
+
+function Card() {
+  return (
+    <div className="card" style={{ width: 18 + 'rem' }}>
+      <div class="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p classNmae="card-text">Card body</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            < Card />
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
